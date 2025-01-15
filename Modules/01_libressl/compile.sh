5f0cf3ddadac3b6211282d2bff1d48b1fbfc21db
Bas van Beek <basvbeek@gmail.com>
2025-01-15 19:52:59 +0100

    Updated libressl to v3.9.2

--- Modules/01_libressl/compile.sh
+++ Modules/01_libressl/compile.sh
@@ -5,7 +5,8 @@ set -e # fail out if any step fails
 . ../../setCompilePath.sh
 
 if [ ! -d portable/.git ]; then
-    git clone https://github.com/libressl-portable/portable.git
+    # v3.9.2 is the last release with mips32 support.
+    git clone -b v3.9.2 https://github.com/libressl-portable/portable.git
     cd portable
 #    git checkout OPENBSD_6_6
     sed -i 's/program_invocation_short_name/"?"/g' crypto/compat/getprogname_linux.c
