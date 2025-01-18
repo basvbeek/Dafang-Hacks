e239cddb3b384ed40470576c25d6e707ed171a34
root <basvbeek@gmail.com>
2025-01-18 14:16:47 +0000

    Added v4l2cpp.

---
+++ v4l2cpp/compile.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+ROOTPATH=$(git rev-parse --show-toplevel)
+echo "setting $ROOTPATH"
+export TOOLCHAIN=${ROOTPATH}/toolchain/bin
+CROSS_PREFIX=mips-linux-gnu-
+export CC=${TOOLCHAIN}/${CROSS_PREFIX}gcc
+export LD=${TOOLCHAIN}/${CROSS_PREFIX}ld
+export CCLD=${TOOLCHAIN}/${CROSS_PREFIX}ld
+export CXX=${TOOLCHAIN}/${CROSS_PREFIX}g++
+export CPP=${TOOLCHAIN}/${CROSS_PREFIX}cpp
+export CXXCPP=${TOOLCHAIN}/${CROSS_PREFIX}cpp
+export AR=${TOOLCHAIN}/${CROSS_PREFIX}ar
+export CROSS_COMPILE=$TOOLCHAIN/mips-linux-gnu-
+export PKG_CONFIG_PATH=$ROOTPATH/_install/lib/pkgconfig
+export LIBRARY_PATH=$ROOTPATH/_install/lib
+export CFLAGS="-muclibc -O3 -lrt -I../_install/include/ ${NO_MOTION_SYSTEM_CALLS}"
+export CPPFLAGS="-muclibc -O3 -lrt -I../_install/include/ -std=c++11 ${NO_MOTION_SYSTEM_CALLS}"
+export LDFLAGS="-muclibc -O3 -L${LIBRARY_PATH}  -lpthread"
+export INSTALLDIR=${ROOTPATH}/_install
+export INSTALL=install-sh
+
+rm CMakeCache.txt
+rm -r CMakeFiles
+cmake -DCMAKE_TOOLCHAIN_FILE="./dafang.toolchain"  -DCMAKE_INSTALL_PREFIX=./_install --debug-output && make VERBOSE=1 -j4 install
+
