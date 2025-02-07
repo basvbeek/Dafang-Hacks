c83cadcd4c9954635490ec6cd6300b67241a600d
Bas van Beek <basvbeek@gmail.com>
2025-02-07 15:15:15 +0100

    Add modules install.

--- kernel/compile.sh
+++ kernel/compile.sh
@@ -4,9 +4,10 @@ set -e # fail out if any step fails
 
 . ../setCompilePath.sh
 
-export INSTALL_MOD_PATH=${INSTALLDIR}/lib/modules/
+export INSTALL_MOD_PATH=${INSTALLDIR}
 
 make clean
 make uImage -j8
 make modules
 make headers_install
+make modules_install
